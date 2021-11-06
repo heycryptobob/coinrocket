fb37c06a6b28375f9656463753dabbc955efb0e3
heycryptobob <heycryptobob@gmail.com>
2021-11-06 23:45:43 +0100

    fix shadow

--- src/components/Advert.js
+++ src/components/Advert.js
@@ -1,10 +1,12 @@
-import { Image } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export function Advert({ ad }) {
   console.log(ad)
   return (
     <a href={ad.url}>
-      <Image src={ad.image_url} alt={ad.name} className="shadow-sm" fluid />
+      <Card className="shadow" bg="transparent">
+        <Card.Img src={ad.image_url} alt={ad.name} />
+      </Card>
     </a>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,4 +16,4 @@ export function HomePage() {
       </Row>
     </Container>
   )
-}//)}
+}
